5132ee191b03d5553e47d5c6da02921172a88299
animonte <montene.ani@gmail.com>
2023-07-08 01:18:44 +0200

    Agrega lectura de dataset y convierte a data frame

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-import gradio as gr
+# import gradio as gr
+import pandas as pd 
+from datasets import load_dataset
 
-def greet(name):
-    return "Hello " + name
+# df = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+# df['target'] = pd.Series(dataset.target)
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+# print(df)
 
-demo.launch()
+# def greet(name):
+#     return "Hello " + name
+
+# demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+# demo.launch()
+
+# Lectura del CSV desde un data set
+dataset = load_dataset("animonte/bank-state-data")
+df = pd.DataFrame(dataset["train"])
+ 
+print(df)
