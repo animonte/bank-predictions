2e76177f52ec46cd444efd2e7de8ba878d1acbf5
animonte <montene.ani@gmail.com>
2023-07-07 20:00:50 +0200

    Agrega primer ejemplo con gradio

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Mi primer prueba con Huggingface")
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+demo.launch()
